e1ae70e7630aaf10c51aae9cdb88b4d6f3ebdc5f
eliosinhache <elioh93@gmail.com>
2021-10-26 23:05:13 -0300

    Sound And Bullet Activity

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public float speed;
+    private void Update()
+    {
+        //Esta sencilla función hará que la bala avance hacia adelante
+        transform.position = transform.position + transform.up * speed;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.LogWarning("colisiona con algo");
+        if (col.collider.name!= "Ship")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);            
+        }
+    }
+}
--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -7,6 +7,8 @@ public class ShipController : MonoBehaviour
 {
     private new Rigidbody2D rigidbody;
     private Vector2 movement;
+    public AudioSource shootingSound;
+    public GameObject bullet;
 
     public float speed = 1f;
 
@@ -22,6 +24,15 @@ public class ShipController : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         movement = new Vector2(moveHorizontal, 0f);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Acá lo importante es aprender la función Instatiate(). En este caso
+            // el primer parámetro el Prefab que queremos instanciar, el segundo
+            // parámetro es la posición donde lo vamos instanciar (en este caso la posición de la nave)
+            // y el tercero es la rotación del Prefab que se está instanciando.
+            Instantiate(bullet, transform.position + new Vector3(0f, 0.7f, 0f), transform.rotation);
+            shootingSound.Play();
+        }
     }
 
     // FixedUpdate se llama en cada fixed frame-rate frame. (50 llamadas por segundo, por defecto)
